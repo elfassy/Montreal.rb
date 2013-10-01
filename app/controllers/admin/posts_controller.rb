18aadaf948617d60d247ce5cf936afa644a56e13
Alexandre Croteau-Pothier <alex@motioneleven.com>
2013-09-30 21:11:46 -0400

    organising post controller

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -69,6 +69,6 @@ class Admin::PostsController < Admin::BaseController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
-    params.require(:post).permit(:title, :text, :tags)
+    params.require(:post).permit(:title, :content, :tags)
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -7,6 +7,10 @@ describe HomeController do
       get 'index'
       response.should be_success
     end
+    it "returns all the posts" do
+      get 'index',{}
+      assigns(:posts).should eq([])
+    end
   end
 
 end
